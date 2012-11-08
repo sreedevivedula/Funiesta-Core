33c8acf8dc919e462bc6cf0b8dd67e3159bf412d
Shibha Malik <Shibha@shibhamacggn.corporate.thoughtworks.com>
2012-11-08 15:00:10 +0530

    Funiesta

---
+++ modules/Funiesta-Web/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+package main.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shibha
+ * Date: 11/8/12
+ * Time: 2:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("hi ");
+
+    }
+}
---
+++ modules/login/src/main/java/Login.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shibha
+ * Date: 11/8/12
+ * Time: 2:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Login {
+
+
+}
